057560f228dc678d9871e4b9dea5827582da9318
vasanth-mvvr <veeravasanthreddy2003@gmail.com>
2025-08-06 21:34:51 +0530

    latest

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "db" {
-    ami = "ami-090252cbe067a9e58"
+    ami = "ami-09c813fb71547fc4f"
     instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow_ssh.id]
     tags = {
