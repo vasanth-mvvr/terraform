a85b38213e1504a20e2ef28a1e6c9786636f66e9
vasanth-mvvr <veeravasanthreddy2003@gmail.com>
2025-08-07 18:57:41 +0530

    latest

--- expense/r53.tf
+++ expense/r53.tf
@@ -1,4 +1,4 @@
-resource "aws_route53_record" "vasanthreddy.space" {
+resource "aws_route53_record" "expense" {
     count = length(var.instance_names)
     zone_id = var.zone_id
     name = var.instance_names[count.index] == "frontend" ? var.domain_name : "${var.instance_names[count.index]}.${var.domain_name}"
