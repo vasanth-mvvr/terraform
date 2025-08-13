7cd358f5c9665c9d9a9d3fadaf7a0f088a728ad1
vasanth-mvvr <veeravasanthreddy2003@gmail.com>
2025-08-13 22:44:44 +0530

    latest

--- terraform_aws_vpc/peering.tf
+++ terraform_aws_vpc/peering.tf
@@ -4,3 +4,31 @@ resource "aws_vpc_peering_connection" "vpc" {
   peer_vpc_id = var.acceptor_vpc_id == "" ? data.aws_vpc.default.id : var.acceptor_vpc_id   
   auto_accept = var.acceptor_vpc_id == "" ? true : false
 }
+
+resource "aws_route" "public_peering" {
+  count = var.is_peering_required && var.acceptor_vpc_id== "" ? 1 : 0
+  route_table_id = aws_route_table.public.id
+  destination_cidr_block = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.vpc[0].id
+}
+
+resource "aws_route" "private_peering" {
+  count = var.is_peering_required && var.acceptor_vpc_id == "" ? 1: 0
+  route_table_id = aws_route_table.private.id
+  destination_cidr_block = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.vpc[0].id
+}
+
+resource "aws_route" "database-peering" {
+  count = var.is_peering_required && var.acceptor_vpc_id== "" ?1:0
+  route_table_id = aws_route.database.id
+  destination_cidr_block = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.vpc[0].id
+}
+
+resource "aws_route" "default-peering" {
+  count = var.is_peering_required && var.acceptor_vpc_id == "" ? 1 : 0
+  route_table_id = data.aws_route_table.main.id
+  destination_cidr_block = var.vpc_cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.vpc[0].id
+}
